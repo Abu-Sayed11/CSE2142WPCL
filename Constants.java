7ffa0fe940e58bee0509d67ee9908c92ad9cd9ce
abu sayed <81965394+Abu-Sayed11@users.noreply.github.com>
2022-06-21 18:37:04 +0600

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,15 @@
+public class Constants {
+    public static final String studentList = "students.txt";
+    public static final String showAll = "-a";
+    public static final String showCount = "-c";
+    public static final String findFor = "?";
+    public static final String addSomething = "+";
+    public static final String randomName = "-r";
+    public static final String comma = ",";
+    public static final String usage = "Usage: -a|-r|-c|+Word|?Word";
+    public static final String dateFormatString = "dd/mm/yyyy-hh:mm:ss a";
+    public static final String loadData = "Loading data ...";
+    public static final String loadedData = "Data Loaded.";
+    public static final String found = "We found it!";
+
+}

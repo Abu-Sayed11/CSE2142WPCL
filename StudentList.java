3faeef260926b3f2f9f4fcf686a172aef92073ce
abu sayed <81965394+Abu-Sayed11@users.noreply.github.com>
2022-08-17 23:57:29 +0600

    Update StudentList.java

--- StudentList.java
+++ StudentList.java
@@ -4,101 +4,84 @@ import java.util.*;
 
 public class StudentList {
 	public static void main(String[] args) {
-
-		// Check arguments
-		if (args.length == 0 || args.length != 1) {
-			System.out.println(Constants.usage);
+		if (args.length != 1) { // Checking arguments available or not
+			System.out.println(Constants.USAGE);
 			return;
-		}
-		if (args[0].equals(Constants.showAll)) {
-			System.out.println(Constants.loadData);
-			try {
-				BufferedReader bufferedReader = readFile();
-				String names[] = bufferedReader.readLine().split(Constants.comma);
-				for (String name : names) {
-					System.out.println(name.trim());
-				}
-				bufferedReader.close();
-			} catch (Exception e) {
 
+		} else if (args[0].equals(Constants.SHOW_ALL)) {
+			System.out.println(Constants.DATA_LOADING);
+			String names[] = fileToStirngArray(Constants.STUDENT_LIST);
+			for (String name : names) {
+				System.out.println(name.trim());
 			}
-			System.out.println(Constants.loadedData);
-		} else if (args[0].equals(Constants.randomName)) {
-			System.out.println(Constants.loadData);
-			try {
-				BufferedReader bufferedReader = readFile();
-				String names[] = bufferedReader.readLine().split(Constants.comma);
-				Random random = new Random();
-				System.out.println(names[random.nextInt(4)].trim());
-				bufferedReader.close();
-			} catch (Exception e) {
+			System.out.println(Constants.DATA_LOADED);
 
-			}
-			System.out.println(Constants.loadedData);
-		} else if (args[0].contains(Constants.addSomething)) {
-			System.out.println(Constants.loadData);
+		} else if (args[0].equals(Constants.RANDOM)) {
+			System.out.println(Constants.DATA_LOADING);
+			String names[] = fileToStirngArray(Constants.STUDENT_LIST);
+			Random random = new Random();  // random object created for random name generating
+			System.out.println(names[random.nextInt(names.length)].trim());  // random name printing
+			System.out.println(Constants.DATA_LOADED);
+
+		} else if (args[0].contains(Constants.PLUS_SIGN)) {
+			System.out.println(Constants.DATA_LOADING);
 			try {
-				BufferedWriter bufferedWriter = writeFile();
+				BufferedWriter bufferedWriter = writeToFile(Constants.STUDENT_LIST);  // file writer object
 				String student = args[0].substring(1);
 				Date date = new Date();
-				DateFormat dateFormat = new SimpleDateFormat(Constants.dateFormatString);
-				bufferedWriter.write(Constants.comma + " " + student + "\nList last updated on " + dateFormat.format(date));
+				DateFormat dateFormat = new SimpleDateFormat(Constants.DATE_FORMAT_STRING);
+				bufferedWriter.write(Constants.COMMA + " " + student + "\nList last updated on " + dateFormat.format(date));
 				bufferedWriter.close();
 			} catch (Exception e) {
-
+				e.printStackTrace();
 			}
+			System.out.println(Constants.DATA_LOADED);
 
-			System.out.println(Constants.loadedData);
-		} else if (args[0].contains(Constants.findFor)) {
-			System.out.println(Constants.loadData);
-			try {
-				BufferedReader bufferedReader = readFile();
-				String names[] = bufferedReader.readLine().split(Constants.comma);				
-				String studentName = args[0].substring(1);
-				int countNames = 0;
-				for (int index = 0; index < names.length; index++) {
-					if (names[index].trim().equals(studentName)) {
-						countNames = countNames + 1;
-					}
+		} else if (args[0].contains(Constants.QUESTION_MARK)) {
+			System.out.println(Constants.DATA_LOADING);
+			String names[] = fileToStirngArray(Constants.STUDENT_LIST);			
+			String studentName = args[0].substring(1);
+			int countNames = 0;
+			for (int index = 0; index < names.length; index++) {
+				if (names[index].trim().equals(studentName)) {
+					countNames = countNames + 1;
 				}
-				if(countNames == 0) {
-					System.out.println("Name not found!");
-				}
-				else {
-					System.out.println(countNames + " name found!");
-				}
-				bufferedReader.close();
-			} catch (Exception e) {
-
 			}
-			System.out.println(Constants.loadedData);
-		} else if (args[0].contains(Constants.showCount)) {
-			System.out.println(Constants.loadData);
-			try {
-				BufferedReader bufferedReader = readFile();
-				String words[] = bufferedReader.readLine().split(Constants.comma);
-				// names.length means total number of words which is splitted by comma.
-				System.out.println(words.length + " words found!");
-				bufferedReader.close();
-			} catch (Exception e) {
-
+			if(countNames == 0) {
+				System.out.println(Constants.NAME_NOT_FOUND_MESSAGE);
 			}
-			System.out.println(Constants.loadedData);
+			else {
+				System.out.println(countNames + " data " + Constants.NAME_FOUND_MESSAGE);
+			}
+			System.out.println(Constants.DATA_LOADED);
+
+		} else if (args[0].contains(Constants.SHOW_COUNT)) {
+			System.out.println(Constants.DATA_LOADING);
+			String words[] = fileToStirngArray(Constants.STUDENT_LIST);
+			System.out.println(words.length + Constants.WORD_FOUND_MESSAGE);  // words.length returns total number of words available in the file
+			System.out.println(Constants.DATA_LOADED);
+
 		} else {
-			System.out.println(Constants.usage);
+			System.out.println(Constants.INVALID_ARGUMENT_MESSAGE); // No arguments matches
+			System.out.println(Constants.USAGE);
 		}
 	}
-
-	private static BufferedWriter writeFile() throws IOException {
-		BufferedWriter bufferedReader = new BufferedWriter(
-				new FileWriter(Constants.studentList, true));
-		return bufferedReader;
+	private static BufferedWriter writeToFile(String fileName) throws IOException {  // returns a buffered file writer
+		BufferedWriter bufferedWriter = new BufferedWriter(
+				new FileWriter(fileName, true));
+		return bufferedWriter;
 	}
-
-	private static BufferedReader readFile() throws FileNotFoundException {
-		BufferedReader bufferedReader = new BufferedReader(
+	private static String[] fileToStirngArray(String fileName) {  // reading from file and returning as string array
+		try {
+			BufferedReader bufferedReader = new BufferedReader(
 				new InputStreamReader(
-						new FileInputStream(Constants.studentList)));
-		return bufferedReader;
+						new FileInputStream(fileName)));
+			String temp[] = bufferedReader.readLine().split(Constants.COMMA);
+			bufferedReader.close();
+			return temp;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }

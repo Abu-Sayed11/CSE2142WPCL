370adfebbcf695271d21c4127a17496bbf803940
abu sayed <81965394+Abu-Sayed11@users.noreply.github.com>
2022-08-17 23:55:10 +0600

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,15 +1,18 @@
 public class Constants {
-    public static final String studentList = "students.txt";
-    public static final String showAll = "-a";
-    public static final String showCount = "-c";
-    public static final String findFor = "?";
-    public static final String addSomething = "+";
-    public static final String randomName = "-r";
-    public static final String comma = ",";
-    public static final String usage = "Usage: -a|-r|-c|+Word|?Word";
-    public static final String dateFormatString = "dd/mm/yyyy-hh:mm:ss a";
-    public static final String loadData = "Loading data ...";
-    public static final String loadedData = "Data Loaded.";
-    public static final String found = "We found it!";
+    public static final String STUDENT_LIST = "students.txt";
+    public static final String SHOW_ALL = "a";
+    public static final String SHOW_COUNT = "c";
+    public static final String QUESTION_MARK = "?";
+    public static final String PLUS_SIGN = "+";
+    public static final String RANDOM = "r";
+    public static final String COMMA = ",";
+    public static final String USAGE = "Usage:\nuse 'a' for Show All\nUse 'r' for print a random name\nUse 'c' for Count all words\nUse '+Word' for Add a word in the list\nUse '?Word' for Search a word";
+    public static final String DATE_FORMAT_STRING = "dd/mm/yyyy-hh:mm:ss a";
+    public static final String DATA_LOADING = "Loading data ...";
+    public static final String DATA_LOADED = "Data Loaded.";
+    public static final String INVALID_ARGUMENT_MESSAGE = "Invalid Argument\n";
+    public static final String NAME_NOT_FOUND_MESSAGE = "Name Not Found!";
+    public static final String NAME_FOUND_MESSAGE = "Found!";
+    public static final String WORD_FOUND_MESSAGE = " words found!";
 
 }
